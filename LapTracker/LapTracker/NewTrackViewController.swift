9192d3d51c27b16d7add50e5063bab56395b0076
Jack boyce <boyce.j3131@gmail.com>
2017-01-31 11:49:17 -0500

    Added a location adder

--- LapTracker/LapTracker/NewTrackViewController.swift
+++ LapTracker/LapTracker/NewTrackViewController.swift
@@ -7,13 +7,80 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+import HealthKit
 
-class NewTrackViewController: UIViewController {
-
+class NewTrackViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var map: MKMapView!
+    var seconds = 0.0
+    var distance = 0.0
+    lazy var locationManager: CLLocationManager = {
+        var _locationManager = CLLocationManager()
+        _locationManager.delegate = self
+        _locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        _locationManager.activityType = .other
+        
+        // Movement threshold for new events
+        _locationManager.distanceFilter = 10.0
+        return _locationManager
+    }()
+    
+    lazy var locations = [CLLocation]()
+    lazy var timer = Timer()
+    let regionRadius: CLLocationDistance = 1000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        locationManager.requestAlwaysAuthorization()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
+    }
+    
+    func eachSecond(timer: Timer) {
+        seconds += 1
+        let secondsQuantity = HKQuantity(unit: HKUnit.second(), doubleValue: seconds)
+        tempLabel.text = "Time: " + secondsQuantity.description
+        let distanceQuantity = HKQuantity(unit: HKUnit.meter(), doubleValue: distance)
+        tempLabel.text = tempLabel.text! + "\nDistance: " + distanceQuantity.description
+        
+        let paceUnit = HKUnit.second().unitDivided(by: HKUnit.meter())
+        let paceQuantity = HKQuantity(unit: paceUnit, doubleValue: seconds / distance)
+        tempLabel.text = tempLabel.text! + "\nPace: " + paceQuantity.description
+    }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        map.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        for location in locations {
+            centerMapOnLocation(location: location)
+            print(location.coordinate.latitude)
+            print(location.coordinate.longitude)
+            if location.horizontalAccuracy < 20 {
+                //update distance
+                if self.locations.count > 0 {
+                    distance += location.distance(from: self.locations.last!)
+                }
+                
+                
+                //save location
+                self.locations.append(location)
+            }
+        }
+    }
+    
+    func startLocationUpdates() {
+        // Here, the location manager will be lazily instantiated
+        locationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +88,13 @@ class NewTrackViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func record(_ sender: Any) {
+        seconds = 0.0
+        distance = 0.0
+        locations.removeAll(keepingCapacity: false)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(eachSecond), userInfo: nil, repeats: true)
+        startLocationUpdates()
+    }
 
     /*
     // MARK: - Navigation
